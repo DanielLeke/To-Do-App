c9ae062bad7c82070aa8a76ea0de041dc46f6ba4
Daniel Adeleke <danieladeleke05@gmail.com>
2023-12-18 14:06:32 +0100

    Added some features to make it look more aesthetically pleasing to the user

--- lib/utilities/todo_tile.dart
+++ lib/utilities/todo_tile.dart
@@ -13,11 +13,28 @@ class ToDoTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Checkbox(value: value, onChanged: onChanged),
-      title: Padding(
-        padding: const EdgeInsets.all(25.0),
-        child: Text(title),
+    return Container(
+      margin: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.yellow,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: ListTile(
+        leading: Checkbox(
+          value: value,
+          onChanged: onChanged,
+          activeColor: Colors.green[200],
+        ),
+        title: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Text(
+            title,
+            style: TextStyle(
+                decoration: value == true
+                    ? TextDecoration.lineThrough
+                    : TextDecoration.none),
+          ),
+        ),
       ),
     );
   }
