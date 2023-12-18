2367b8b128fb526032e4b677aad219e90be18743
Daniel Adeleke <danieladeleke05@gmail.com>
2023-12-18 11:38:35 +0100

    Created file todo_tile in folder utilities

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_flutter_project/utilities/todo_tile.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
---
+++ lib/utilities/todo_tile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ToDoTile extends StatelessWidget {
+  const ToDoTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
