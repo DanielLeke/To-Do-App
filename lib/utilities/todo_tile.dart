fdcd0aa42266f8828b95c6e538e15ec8a4eb2433
Daniel Adeleke <danieladeleke05@gmail.com>
2023-12-18 11:46:12 +0100

    Added a list tile and some variables to todo_tile in order for it to be usable by the other widgets

--- lib/utilities/todo_tile.dart
+++ lib/utilities/todo_tile.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
 class ToDoTile extends StatelessWidget {
-  const ToDoTile({super.key});
+  const ToDoTile({super.key, required this.value, required this.onChanged, required this.title});
+
+  final value;
+  final Function(bool?)? onChanged;
+  final title;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: ListTile(
+        leading: Checkbox(value: value, onChanged: onChanged),
+        title: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Text(title),
+        ),
+      ),
+    );
   }
 }
