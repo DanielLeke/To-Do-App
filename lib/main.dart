dd25b772d78d3f2d38040db37c29f90eb5f7a37a
Daniel Adeleke <danieladeleke05@gmail.com>
2023-12-18 11:29:05 +0100

    Added pages folder and put a file home_page in the folder. Also the home_page will be the page the user sees

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_flutter_project/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
         primaryColor: Colors.yellow,
         primarySwatch: Colors.yellow,
       ),
-      home: Scaffold(),
+      home: HomePage(),
     );
   }
 }
