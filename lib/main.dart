9f333596746830620022c9e1b5a8f77206a65a3f
Daniel Adeleke <danieladeleke05@gmail.com>
2023-12-18 11:24:24 +0100

    Defined theme for the app

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        primaryColor: Colors.yellow,
+        primarySwatch: Colors.yellow,
+      ),
+      home: Scaffold(),
+    );
   }
 }
